e5c39bdb986da27834fcefcad7e8ee6768c4dfeb
marabesi <matheus.marabesi@gmail.com>
2017-08-28 21:03:21 -0300

    base structure

---
+++ src/Commands/Generate.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace EasyCrud\Commands;
+
+class Generate
+{
+}
---
+++ src/Providers/EasyCrud.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace EasyCrud\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class EasyCrud extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
