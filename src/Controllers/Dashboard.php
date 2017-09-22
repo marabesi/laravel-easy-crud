4cf75937a23764f002cfc86e33e1f4b413bb3c4c
marabesi <matheus.marabesi@gmail.com>
2017-09-22 11:03:59 -0300

    added middleware feature

--- src/Controllers/Dashboard.php
+++ src/Controllers/Dashboard.php
@@ -7,6 +7,15 @@ use Illuminate\Routing\Controller as Controller;
 class Dashboard extends Controller
 {
 
+    public function __construct()
+    {
+        $middlewares = config('easy-crud.middlewares');
+
+        foreach ($middlewares as $middleware) {
+            $this->middleware($middleware);
+        }
+    }
+
     public function getIndex()
     {
         return view('easy-crud::dashboard');
